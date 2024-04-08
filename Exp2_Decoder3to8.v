46ce3fc7f73f5fb74412899d4ab7f31f2c818b59
Asansiddiq <160508575+Asansiddiq@users.noreply.github.com>
2024-04-08 10:34:24 +0530

    Update Exp2_Decoder3to8.v

--- Exp2_Decoder3to8.v
+++ Exp2_Decoder3to8.v
@@ -1,7 +1,13 @@
 module decoder_8(a,b,c,y);
-input a,b,c;
-output[7:0]y;
-
-
-
+input a,b,c; 
+output[7:0]y; 
+and gl(y[0],(~a),(~b),(~c)); 
+and g2(y[1],(~a),(~b),(c)); 
+and g3(y[2],(~a),(b),(~c));
+and g4(y[3],(~a),(b),(c));
+and g5(y[4],(a),(~b),(~c));
+and g6(y[5],(a), (~b), (c));
+and g7(y[6], (a), (b), (~c)); 
+and g8(y[7], (a), (b), (c));
 endmodule
+
